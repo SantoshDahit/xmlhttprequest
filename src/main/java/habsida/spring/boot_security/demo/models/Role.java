539c25d5401608d560ff316924b051690aa19c62
Santosh Dahit <santoshdahit455@gmail.com>
2024-04-28 23:23:07 +0900

    task 3.1.4

--- src/main/java/habsida/spring/boot_security/demo/models/Role.java
+++ src/main/java/habsida/spring/boot_security/demo/models/Role.java
@@ -16,7 +16,7 @@ public class Role implements GrantedAuthority {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Byte id;
 
-    @Column(unique = true)
+    @Column
     private String role;
 
 
